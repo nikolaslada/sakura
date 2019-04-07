fa821808c0c6095f3875a64b0e125620f9ed2472
Nikolas Lada <nikolas.lada@gmail.com>
2019-04-07 16:47:45 +0200

    Added Traversal\Node class

---
+++ src/Traversal/Node.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sakura\Traversal;
+
+use Sakura\INode;
+
+final class Node implements INode
+{
+
+    /** @var int */
+    private $id;
+    
+    /** @var int */
+    private $left;
+    
+    /** @var int */
+    private $right;
+    
+    /** @var int */
+    private $parent;
+    
+    
+    public function __construct(int $id, int $left, int $right, int $parent)
+    {
+        $this->id = $id;
+        $this->left = $left;
+        $this->right = $right;
+        $this->parent = $parent;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getLeft(): int
+    {
+        return $this->left;
+    }
+
+    public function getRight(): int
+    {
+        return $this->right;
+    }
+
+    public function getParent(): int
+    {
+        return $this->parent;
+    }
+
+}
