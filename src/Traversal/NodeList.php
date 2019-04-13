bc208f2bb4a50d2fb992051e6f741c6779a12fb8
Nikolas Lada <nikolas.lada@gmail.com>
2019-04-13 23:40:08 +0200

    Added Traversal\NodeList domain class

---
+++ src/Traversal/NodeList.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sakura\Traversal;
+
+final class NodeList extends \ArrayIterator
+{
+
+    public function getById(int $id): ?INode
+    {
+        if (isset($this[$id])) {
+            $node = $this[$id];
+            /* @var $node INode */
+            if ($node->getId() !== $id) {
+                throw new \Sakura\Exceptions\RuntimeException("Id of Node is not same as key: $id");
+            }
+
+            return $node;
+        } else {
+            return \null;
+        }
+    }
+
+}
