528cbe26aecd7dc3bd2d156d06bb406c822aee08
Nikolas Lada <nikolas.lada@gmail.com>
2019-08-19 20:33:22 +0200

    Overriden current() method in Recursive\Branch

--- src/Recursive/Branch.php
+++ src/Recursive/Branch.php
@@ -17,6 +17,20 @@ final class Branch extends \ArrayIterator implements IBranch
         parent::__construct($list);
     }
 
+    /**
+     * @throws \RuntimeException
+     */
+    public function current(): Branch
+    {
+        $current = parent::current();
+
+        if ($current instanceof Branch) {
+            return $current;
+        } else {
+            throw new \RuntimeException('current value should be Recursive\Branch and it is not!');
+        }
+    }
+
     public function getRootNode(): INode
     {
         return $this->root;
