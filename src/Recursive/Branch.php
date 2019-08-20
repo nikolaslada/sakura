2b43a061feb4cda41fecfb7de7ff39088fd83a4f
Nikolas Lada <nikolas.lada@gmail.com>
2019-08-20 08:53:10 +0200

    New method existsNode() in Recursive\Branch

--- src/Recursive/Branch.php
+++ src/Recursive/Branch.php
@@ -24,16 +24,51 @@ final class Branch extends \ArrayIterator implements IBranch
     {
         $current = parent::current();
 
-        if ($current instanceof Branch) {
+        if ($current instanceof Branch)
+        {
             return $current;
         } else {
-            throw new \RuntimeException('current value should be Recursive\Branch and it is not!');
+            throw new \RuntimeException;
         }
     }
 
     public function getRootNode(): INode
     {
         return $this->root;
+    }
+
+    public function existsNode(int $nodeId): bool
+    {
+        if ($this->root->getId() === $nodeId) {
+            return true;
+        }
+
+        return self::searchInBranch($this, $nodeId);
+    }
+    
+    private static function searchInBranch(Branch $branch, int $nodeId): bool
+    {
+        while($branch->valid()) {
+            $subBranch = $branch->current();
+            $node = $subBranch->getRootNode();
+
+            if ($node->getId() === $nodeId) {
+                $branch->rewind();
+                return true;
+            }
+
+            $result = self::searchInBranch($subBranch, $nodeId);
+
+            if ($result) {
+                $branch->rewind();
+                return $result;
+            } else {
+                $branch->next();
+            }
+        }
+
+        $branch->rewind();
+        return false;
     }
 
 }
