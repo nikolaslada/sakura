fa845621aaccc663b2bd48ae7da0eeb94b119ca7
Nikolas Lada <nikolas.lada@gmail.com>
2020-01-21 18:54:58 +0100

    Added Traversal\IRepository

--- src/Traversal/IRepository.php
+++ src/Traversal/IRepository.php
@@ -9,26 +9,32 @@ interface IRepository
 
     public function addData(array $data): int;
 
-    public function getNodeById(int $id): INode;
+    public function getNodeById(int $id): ?INode;
 
     public function getNodeByLeft(int $left): ?INode;
 
     public function getNodesByParent(int $parent): NodeList;
 
-    public function getBranch(int $left, int $right): NodeList;
+    public function getLevel(INode $node): ?int;
 
-    public function getPath(INode $id): NodeList;
+    public function getBranch(INode $node): NodeList;
 
-    public function getNumberOfChilds(int $nodeId): int;
+    public function getPath(INode $node, bool $isAscending): NodeList;
 
-    public function updateByIdList(array $whereIdList, ?int $setParent);
+    public function getNumberOfChilds(int $id): int;
 
-    public function updateByLeftRight(int $fromLeft, ?int $toLeft, int $fromRight, ?int $toRight, int $leftMovement, int $rightMovement);
+    public function updateById(int $whereId, ?int $setParent): int;
 
-    public function beginTransaction();
+    public function updateByParent(int $whereParent, ?int $setParent): int;
 
-    public function commitTransaction();
+    public function updateByLeft(int $from, ?int $to, int $movement);
 
-    public function delete(int $id);
+    public function updateByRight(int $from, ?int $to, int $movement);
+
+    public function beginTransaction(): void;
+
+    public function commitTransaction(): void;
+
+    public function delete(int $id): void;
 
 }
